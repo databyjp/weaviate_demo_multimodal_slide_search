fd23a7d2f5b883b44de2a91a93242c1bf70e1fb3
JP Hwang <me@jphwang.com>
2025-04-22 18:47:11 +0100

    Refactor to use WCD

--- app.py
+++ app.py
@@ -8,11 +8,23 @@ from helpers import (
 )
 import weaviate
 from weaviate.classes.query import MetadataQuery
+from weaviate.classes.init import Auth
+from dotenv import load_dotenv
 import os
 
-client = weaviate.connect_to_local(
+# client = weaviate.connect_to_local(
+#     headers={
+#         "X-Cohere-Api-Key": os.environ["COHERE_APIKEY"]
+#     }
+# )
+
+load_dotenv()
+
+client = weaviate.connect_to_weaviate_cloud(
+    cluster_url=os.environ["APP_WEAVIATE_CLOUD_URL"],
+    auth_credentials=Auth.api_key(os.environ["APP_WEAVIATE_CLOUD_APIKEY"]),
     headers={
-        "X-Cohere-Api-Key": os.environ["COHERE_APIKEY"]
+        "X-Cohere-Api-Key": os.environ["APP_COHERE_API_KEY"]
     }
 )
 
